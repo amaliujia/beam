20b0359f8d2f092f0d6d543944237275d2829516
amaliujia <amaliujia@gmail.com>
2018-08-17 16:02:19 -0700

    update

--- sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/BeamComplexTypeTest.java
+++ sdks/java/extensions/sql/src/test/java/org/apache/beam/sdk/extensions/sql/BeamComplexTypeTest.java
@@ -18,11 +18,13 @@
 package org.apache.beam.sdk.extensions.sql;
 
 import com.google.common.collect.ImmutableMap;
+import java.util.Arrays;
 import org.apache.beam.sdk.extensions.sql.impl.BeamSqlEnv;
 import org.apache.beam.sdk.extensions.sql.impl.rel.BeamSqlRelUtils;
 import org.apache.beam.sdk.extensions.sql.meta.provider.ReadOnlyTableProvider;
 import org.apache.beam.sdk.extensions.sql.mock.MockedBoundedTable;
 import org.apache.beam.sdk.schemas.Schema;
+import org.apache.beam.sdk.schemas.Schema.FieldType;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.values.PCollection;
@@ -35,12 +37,21 @@ import org.junit.Test;
 public class BeamComplexTypeTest {
   private static final Schema innerRowSchema =
       Schema.builder().addStringField("one").addInt64Field("two").build();
+  private static final Schema innerRowWithArraySchema =
+      Schema.builder().addStringField("one").addArrayField("array", FieldType.INT64).build();
   private static final Schema nestedRowSchema =
       Schema.builder()
           .addStringField("nonRowfield1")
           .addRowField("RowField", innerRowSchema)
           .addInt64Field("nonRowfield2")
           .addRowField("RowFieldTwo", innerRowSchema)
+          .build();
+  private static final Schema nestedRowWithArraySchema =
+      Schema.builder()
+          .addStringField("field1")
+          .addRowField("field2", innerRowWithArraySchema)
+          .addInt64Field("field3")
+          .addArrayField("field4", FieldType.INT64)
           .build();
   private static final Schema flattenedRowSchema =
       Schema.builder()
@@ -67,7 +78,16 @@ public class BeamComplexTypeTest {
                           .build()),
               "basicRowTestTable",
               MockedBoundedTable.of(Schema.FieldType.row(innerRowSchema), "col")
-                  .addRows(Row.withSchema(innerRowSchema).addValues("innerStr", 1L).build())));
+                  .addRows(Row.withSchema(innerRowSchema).addValues("innerStr", 1L).build()),
+            "complicatedRowTestTable",
+            MockedBoundedTable.of(Schema.FieldType.row(nestedRowWithArraySchema), "col")
+              .addRows(Row.withSchema(nestedRowWithArraySchema)
+              .addValues(
+                  "str",
+                  Row.withSchema(innerRowWithArraySchema).addValues("inner_str_one", Arrays.asList(1L, 2L, 3L)).build(),
+                  4L,
+                  Arrays.asList(5L, 6L)).build())
+            ));
 
   @Rule public transient TestPipeline pipeline = TestPipeline.create();
 
@@ -76,7 +96,7 @@ public class BeamComplexTypeTest {
     BeamSqlEnv sqlEnv = BeamSqlEnv.inMemory(readOnlyTableProvider);
     PCollection<Row> stream =
         BeamSqlRelUtils.toPCollection(
-            pipeline, sqlEnv.parseQuery("SELECT col FROM nestedRowTestTable"));
+            pipeline, sqlEnv.parseQuery("SELECT nestedRowTestTable.col.RowFieldTwo FROM nestedRowTestTable"));
     PAssert.that(stream)
         .containsInAnyOrder(
             Row.withSchema(flattenedRowSchema)
@@ -93,6 +113,17 @@ public class BeamComplexTypeTest {
             pipeline, sqlEnv.parseQuery("SELECT col FROM basicRowTestTable"));
     PAssert.that(stream)
         .containsInAnyOrder(Row.withSchema(innerRowSchema).addValues("innerStr", 1L).build());
+    pipeline.run().waitUntilFinish(Duration.standardMinutes(2));
+  }
+
+  @Test
+  public void testComplicatedRow() {
+    BeamSqlEnv sqlEnv = BeamSqlEnv.inMemory(readOnlyTableProvider);
+    PCollection<Row> stream =
+        BeamSqlRelUtils.toPCollection(
+            pipeline, sqlEnv.parseQuery("SELECT complicatedRowTestTable.col.field2 FROM complicatedRowTestTable"));
+    PAssert.that(stream).containsInAnyOrder();
+        // .containsInAnyOrder(Row.withSchema(Schema.builder().addInt64Field("int64").build()).addValue(2L).build());
     pipeline.run().waitUntilFinish(Duration.standardMinutes(2));
   }
 }
